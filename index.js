5c5406315d830be1f4abc8d9b6dec3e23820c33c
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2022-02-27 23:05:31 +0100

    Add connection timeout

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { Pool, Client } = require('pg');
 const util = require('util');
 
+const CONNECT_TIMEOUT = 5000;
+
 let argv = require('minimist')(process.argv.slice(2));
 let command = argv._[0];
 
@@ -11,11 +13,20 @@ let command = argv._[0];
     password: argv.w,
     database: argv.d,
     port: argv.p,
+    connectionTimeoutMillis: argv.t == null ? CONNECT_TIMEOUT : argv.t
   });
 
   let fromBlockHeight = parseInt(argv.s || 1);
 
-  client.connect();
+  try {
+    await util.promisify(client.connect).call(client);
+  } catch (error) {
+    console.error(
+      `Failed to connect to database because of error: ${error.message}`
+    );
+    process.exit(1);
+    return;
+  }
 
   if (command === 'prune') {
     if (argv._.length < 2) {
@@ -29,7 +40,7 @@ let command = argv._[0];
     for (let i = fromBlockHeight; i < toBlockHeight; i++) {
       let signaturesBase64;
       try {
-        let res = await util.promisify(client.query).call(client, 'SELECT signatures from blocks where height=$1', [i]);
+        let res = await util.promisify(client.query).call(client, 'SELECT signatures FROM blocks WHERE height=$1', [i]);
         signaturesBase64 = res.rows[0].signatures;
       } catch (error) {
         console.error(error.message);
@@ -49,7 +60,7 @@ let command = argv._[0];
       let prunedSignaturesBase64 = Buffer.from(JSON.stringify(prunedSignatureList), 'utf8').toString('base64');
 
       try {
-        await util.promisify(client.query).call(client, 'UPDATE blocks SET signatures=$1 where height=$2', [prunedSignaturesBase64, i]);
+        await util.promisify(client.query).call(client, 'UPDATE blocks SET signatures=$1 WHERE height=$2', [prunedSignaturesBase64, i]);
       } catch (error) {
         console.error(error.message);
         process.exit(1);
